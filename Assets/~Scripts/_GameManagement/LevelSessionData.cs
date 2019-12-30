b45a2c94766cb8a383e1e649ae4ac0d0e471b454
Cathal McCosker <cathal.mccosker@gmail.com>
2019-12-30 15:57:20 -0800

    AutoCommit

--- Assets/~Scripts/_GameManagement/LevelSessionData.cs
+++ Assets/~Scripts/_GameManagement/LevelSessionData.cs
@@ -20,9 +20,11 @@ public class LevelSessionData : MonoBehaviour
 
     public int numberOfComrades;
     public float comradeFillSpeed = 1;
+
     public int numberOfFarmers = 1;
     public float farmerFillSpeed = 0.5f;
     public int farmerMultiplier = 1;
+
     public int numberOfPotatoes;
     public float potatoFillSpeed = 0.5f;
 
