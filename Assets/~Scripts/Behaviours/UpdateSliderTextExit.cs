832abb91fbd394df2cbec6d1df12c1fb9ebe2317
Cathal McCosker <cathal.mccosker@gmail.com>
2019-12-30 13:26:15 -0800

    AutoCommit

--- Assets/~Scripts/Behaviours/UpdateSliderTextExit.cs
+++ Assets/~Scripts/Behaviours/UpdateSliderTextExit.cs
@@ -44,13 +44,15 @@ public class UpdateSliderTextExit : StateMachineBehaviour
         if (updateNumberOfPotatoes != null)
             updateNumberOfPotatoes();
 
-        if (LevelSessionData.Singleton.numberOfPotatoes < 10 || LevelSessionData.Singleton.numberOfComrades < 1)
+        if (LevelSessionData.Singleton.numberOfPotatoes < 10 * MenuManager.Singleton.farmerMultiplier 
+            || LevelSessionData.Singleton.numberOfComrades < 1 * MenuManager.Singleton.farmerMultiplier)
         {
             if (lockBuyFarmersButton != null)
                 lockBuyFarmersButton();
         }
 
-        if (LevelSessionData.Singleton.numberOfPotatoes >= 10 && LevelSessionData.Singleton.numberOfComrades >= 1)
+        if (LevelSessionData.Singleton.numberOfPotatoes >= 10 * MenuManager.Singleton.farmerMultiplier 
+                && LevelSessionData.Singleton.numberOfComrades >= 1 * MenuManager.Singleton.farmerMultiplier)
         {
             if (unlockBuyFarmersButton != null)
                 unlockBuyFarmersButton();
