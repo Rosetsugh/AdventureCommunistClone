3a3f04f67213a67fe856cc2cba3162ce9a130f64
Cathal McCosker <cathal.mccosker@gmail.com>
2019-12-29 22:10:14 -0800

    AutoCommit

--- Assets/~Scripts/UI/MenuManager.cs
+++ Assets/~Scripts/UI/MenuManager.cs
@@ -1,18 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator potatoTimer;
 
-    // Update is called once per frame
-    void Update()
+    public void UI_Button_DigPotatoes()
     {
-        
+        potatoTimer.SetTrigger("MoveSlider");
     }
 }
