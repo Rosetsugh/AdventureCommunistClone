890c81eb21b9450ab7c767bf9ffea82c988a694c
Cathal McCosker <cathal.mccosker@gmail.com>
2019-12-29 22:47:28 -0800

    AutoCommit

--- Assets/ButtonManager.cs
+++ Assets/ButtonManager.cs
@@ -6,15 +6,22 @@ public class ButtonManager : MonoBehaviour
     private void OnEnable()
     {
         UpdateSliderTextExit.unlockBuyFarmersButton += UnlockBuyFarmersButtonEVH;
+        UpdateSliderTextExit.lockBuyFarmersButton += LockBuyFarmersButtonEVH;
     }
 
     private void OnDisable()
     {
         UpdateSliderTextExit.unlockBuyFarmersButton -= UnlockBuyFarmersButtonEVH;
+        UpdateSliderTextExit.lockBuyFarmersButton -= LockBuyFarmersButtonEVH;
     }
 
     private void UnlockBuyFarmersButtonEVH()
     {
         GetComponent<Button>().interactable = true;
+    }
+
+    private void LockBuyFarmersButtonEVH()
+    {
+        GetComponent<Button>().interactable = false;
     }
 }
