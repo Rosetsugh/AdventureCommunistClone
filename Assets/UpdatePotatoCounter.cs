7579f563f4a228ccd30fdc81fb16b10193b85aa7
Cathal McCosker <cathal.mccosker@gmail.com>
2019-12-29 23:06:18 -0800

    AutoCommit

--- Assets/UpdatePotatoCounter.cs
+++ Assets/UpdatePotatoCounter.cs
@@ -1,6 +1,20 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class UpdatePotatoCounter : MonoBehaviour 
 {
+    private void OnEnable()
+    {
+        UpdateSliderTextExit.updateNumberOfPotatoes += UpdatePotatoCounterEVH;
+    }
 
+    private void OnDisable()
+    {
+        UpdateSliderTextExit.updateNumberOfPotatoes += UpdatePotatoCounterEVH;
+    }
+
+    private void UpdatePotatoCounterEVH()
+    {
+        GetComponent<TextMeshProUGUI>().text = LevelSessionData.Singleton.numberOfPotatoes.ToString();
+    }
 }
