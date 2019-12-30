e467f23bc94719deebab1f96568e48c29f232b79
Cathal McCosker <cathal.mccosker@gmail.com>
2019-12-29 18:57:25 -0800

    AutoCommit

--- Assets/~Scripts/Behaviours/SetSliderSpeedEnter.cs
+++ Assets/~Scripts/Behaviours/SetSliderSpeedEnter.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SetSliderSpeedEnter : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.speed = animator.GetInteger("SliderSpeed");
     }
