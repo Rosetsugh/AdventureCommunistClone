0cca79129d75069c54966d9a7e3d2b849414dc00
Cathal McCosker <cathal.mccosker@gmail.com>
2019-12-29 22:19:45 -0800

    AutoCommit

--- Assets/UpdateSliderSpeedEnter.cs
+++ Assets/UpdateSliderSpeedEnter.cs
@@ -2,5 +2,15 @@
 
 public class UpdateSliderSpeedEnter : StateMachineBehaviour 
 {
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        var countType = animator.GetComponent<SliderCountType>().countType;
 
+        if (countType == "Comrade")
+            animator.SetFloat("SliderSpeed", LevelSessionData.Singleton.comradeFillSpeed);
+
+
+        if (countType == "Potato")
+            animator.SetFloat("SliderSpeed", LevelSessionData.Singleton.potatoFillSpeed);
+    }
 }
