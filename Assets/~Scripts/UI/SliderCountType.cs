c285e6867e69fc7da3a2b14d840b7f4289fd1707
Cathal McCosker <cathal.mccosker@gmail.com>
2019-12-29 23:13:23 -0800

    AutoCommit

--- Assets/~Scripts/UI/SliderCountType.cs
+++ Assets/~Scripts/UI/SliderCountType.cs
@@ -1,7 +1,20 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class SliderCountType : MonoBehaviour
 {
     [StringDropdown("Comrade", "Farmer", "Potato")]
     public string countType;
+
+    private void Start()
+    {
+        if (countType == "Comrade")
+            GetComponentInChildren<TextMeshProUGUI>().text = LevelSessionData.Singleton.numberOfComrades.ToString();
+
+        if (countType == "Farmer")
+            GetComponentInChildren<TextMeshProUGUI>().text = LevelSessionData.Singleton.numberOfFarmers.ToString();
+
+        if (countType == "Potato")
+            GetComponentInChildren<TextMeshProUGUI>().text = LevelSessionData.Singleton.numberOfPotatoes.ToString();
+    }
 }
