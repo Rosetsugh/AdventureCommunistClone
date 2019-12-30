3a728e5055960a4f9d1323245f85e0428829755e
Cathal McCosker <cathal.mccosker@gmail.com>
2019-12-29 22:45:25 -0800

    AutoCommit

--- Assets/ButtonManager.cs
+++ Assets/ButtonManager.cs
@@ -1,6 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonManager : MonoBehaviour 
 {
+    private void OnEnable()
+    {
+        UpdateSliderTextExit.unlockBuyFarmersButton += UnlockBuyFarmersButtonEVH;
+    }
 
+    private void OnDisable()
+    {
+        UpdateSliderTextExit.unlockBuyFarmersButton -= UnlockBuyFarmersButtonEVH;
+    }
+
+    private void UnlockBuyFarmersButtonEVH()
+    {
+        GetComponent<Button>().interactable = true;
+    }
 }
