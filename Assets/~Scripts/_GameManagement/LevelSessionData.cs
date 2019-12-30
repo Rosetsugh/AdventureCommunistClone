38b7b51e41f39018da40c0956add6bac4da5b064
Cathal McCosker <cathal.mccosker@gmail.com>
2019-12-29 22:36:37 -0800

    Implementing Buy Farmers Button

--- Assets/~Scripts/_GameManagement/LevelSessionData.cs
+++ Assets/~Scripts/_GameManagement/LevelSessionData.cs
@@ -18,7 +18,7 @@ public class LevelSessionData : MonoBehaviour
 
     public int numberOfComrades;
     public float comradeFillSpeed = 1;
-    public int numberOfFarmers;
+    public int numberOfFarmers = 1;
     public float farmerFillSpeed = 0.5f;
     public int numberOfPotatoes;
     public float potatoFillSpeed = 0.5f;
@@ -26,7 +26,7 @@ public class LevelSessionData : MonoBehaviour
     private void Start()
     {
         numberOfComrades = 0;
-        numberOfFarmers = 0;
+        numberOfFarmers = 1;
         numberOfPotatoes = 0;
     }
 }
