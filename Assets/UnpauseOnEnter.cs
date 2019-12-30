f796c287a3a96a764d965c5e034276fbdbf11930
Cathal McCosker <cathal.mccosker@gmail.com>
2019-12-30 00:32:32 -0800

    AutoCommit

--- Assets/UnpauseOnEnter.cs
+++ Assets/UnpauseOnEnter.cs
@@ -2,5 +2,8 @@
 
 public class UnpauseOnEnter : StateMachineBehaviour 
 {
-
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+        Time.timeScale = 1;
+    }
 }
