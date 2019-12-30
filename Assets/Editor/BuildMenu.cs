8c84181672e51bbeb5a4a2119b202f4c8d1ad370
Cathal McCosker <cathal.mccosker@gmail.com>
2019-12-29 18:14:06 -0800

    AutoCommit

---
+++ Assets/Editor/BuildMenu.cs
@@ -0,0 +1,301 @@
+﻿using Debug = UnityEngine.Debug;
+
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using System.Diagnostics;
+using System.IO;
+
+public class BuildMenu : MonoBehaviour
+{
+	[MenuItem("Build/Open Android Build Folder")]
+	public static void OpenAndroidBuildFolder()
+	{
+		// Due to an oddity in RevealInFinder we need to put a containing folder address in the folder we want to navigate
+		// to otherwise it will just take us to the Build folder
+		EditorUtility.RevealInFinder(@"\\192.168.1.250\ServerDisk1\_Build\Android\");
+	}
+
+	// The APK is faster to build than the AAB so it should be used for testing, and the AAB for publishing
+	[MenuItem("Build/APK (ServerDisk1\\_Build)")]
+	public static void AndroidBuildAPK()
+	{
+		// First we check for any existing .apk file and delete it (you can run into problems with the build not overwriting
+		// the older file). So if directory exists then we need to delete out the .apk
+		var previousAPKFileLocation = new DirectoryInfo(@"Build\Android\");
+		if (previousAPKFileLocation.Exists)
+		{
+			var filePaths = previousAPKFileLocation.GetFiles("*.apk");
+
+			foreach (var file in filePaths)
+			{
+				file.Delete();
+			}
+		}
+
+		// Tweaks to speed up APK build
+
+		//Set appBundle to false
+		EditorUserBuildSettings.buildAppBundle = false;
+		// Set Scripting to Mono
+		PlayerSettings.SetScriptingBackend(BuildTargetGroup.Android, ScriptingImplementation.Mono2x);
+		// Set build to x86 (works with Genymotion, ARM does not)
+		PlayerSettings.Android.targetArchitectures = AndroidArchitecture.X86;
+
+		BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+		buildPlayerOptions.scenes = FindActiveScenesInBuildSettings();
+		buildPlayerOptions.locationPathName = @"Build\Android\" + Application.productName + ".apk";
+		buildPlayerOptions.target = BuildTarget.Android;
+		buildPlayerOptions.options = BuildOptions.None;
+
+		BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+		BuildSummary summary = report.summary;
+
+		if (summary.result == BuildResult.Succeeded)
+		{
+			// We must use UnityEngine.Debug as we are using System.Diagnositics in order to run an external .exe file
+			UnityEngine.Debug.Log("Build succeeded!");
+			// Copy the file to the server (this is easier for the Android device to download from)
+			File.Copy(@"Build\Android\" + Application.productName + ".apk", @"\\192.168.1.250\ServerDisk1\_Build\Android\"
+				+ Application.productName + ".apk", true);
+		}
+
+		if (summary.result == BuildResult.Failed)
+			UnityEngine.Debug.Log("***ANDROID BUILD FAILED!***");
+	}
+
+	// Google Play requires AAB files instead of APK files *** You must enable Build App Bundle in the Build Settings ***
+	[MenuItem("Build/AAB (ServerDisk1\\_Build)")]
+	public static void AndroidBuildAAB()
+	{
+		// First we check for any existing .aab file and delete it (you can run into problems with the build not overwriting
+		// the older file). So if directory exists then we need to delete out the .aab
+		var previousAABFileLocation = new DirectoryInfo(@"Build\Android\");
+		if (previousAABFileLocation.Exists)
+		{
+			var filePaths = previousAABFileLocation.GetFiles("*.aab");
+
+			foreach (var file in filePaths)
+			{
+				file.Delete();
+			}
+		}
+
+		// Build Options for AAB (Google Play)
+
+		// Add as appBundle
+		EditorUserBuildSettings.buildAppBundle = true;
+		// Set Scripting to Mono
+		PlayerSettings.SetScriptingBackend(BuildTargetGroup.Android, ScriptingImplementation.IL2CPP);
+		// Set architecture to ARM64 or ARM7 for deployment to Google Play
+		PlayerSettings.Android.targetArchitectures = AndroidArchitecture.ARM64 | AndroidArchitecture.ARMv7;
+
+		BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+		buildPlayerOptions.scenes = FindActiveScenesInBuildSettings();
+		buildPlayerOptions.locationPathName = @"Build\Android\" + Application.productName + ".aab";
+		buildPlayerOptions.target = BuildTarget.Android;
+		buildPlayerOptions.options = BuildOptions.None;
+
+		BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+		BuildSummary summary = report.summary;
+
+		if (summary.result == BuildResult.Succeeded)
+		{
+			// We must use UnityEngine.Debug as we are using System.Diagnositics in order to run an external .exe file
+			Debug.Log("Build succeeded!");
+			// Copy the file to the server (this is easier for the Android device to download from)
+			File.Copy(@"Build\Android\" + Application.productName + ".aab", @"\\192.168.1.250\ServerDisk1\_Build\Android\"
+				+ Application.productName + ".aab", true);
+		}
+
+		if (summary.result == BuildResult.Failed)
+		{
+			Debug.Log("***ANDROID BUILD FAILED!***");
+		}
+	}
+
+	// The APK is faster to build than the AAB so it should be used for testing, and the AAB for publishing
+	[MenuItem("Build/Build APK and Launch Emulator")]
+	public static void BuildAndRunAPK()
+	{
+		// Check if the 'player.exe' process is already running, if not then launch Genymoton
+		// When using GetProcessByName we don't inlcude the .exe and it is case sensitive (find the process with TaskManager first)
+		if (Process.GetProcessesByName("player").Length == 0)
+		{
+			// Immediately launch the Android Emulator (it should finish booting before the Build is finsihed
+			var launchAndroidEmulator = new Process();
+			launchAndroidEmulator.StartInfo.FileName = @"C:\Program Files\Genymobile\Genymotion\player";
+			launchAndroidEmulator.StartInfo.Arguments = " --vm-name \"Google Nexus - Landscape\"";
+
+			launchAndroidEmulator.Start();
+		}
+		// First we check for any existing .apk file and delete it (you can run into problems with the build not overwriting
+		// the older file). So if directory exists then we need to delete out the .apk
+		var previousAPKFileLocation = new DirectoryInfo(@"Build\Android\");
+		if (previousAPKFileLocation.Exists)
+		{
+			var filePaths = previousAPKFileLocation.GetFiles("*.apk");
+
+			foreach (var file in filePaths)
+			{
+				file.Delete();
+			}
+		}
+
+		// Tweaks to speed up APK build
+
+		//Set appBundle to false
+		EditorUserBuildSettings.buildAppBundle = false;
+		// Set Scripting to Mono
+		PlayerSettings.SetScriptingBackend(BuildTargetGroup.Android, ScriptingImplementation.Mono2x);
+		// Set build to x86 (works with Genymotion, ARM does not)
+		PlayerSettings.Android.targetArchitectures = AndroidArchitecture.X86;
+
+		BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+		buildPlayerOptions.scenes = FindActiveScenesInBuildSettings();
+		buildPlayerOptions.locationPathName = @"Build\Android\" + Application.productName + ".apk";
+		buildPlayerOptions.target = BuildTarget.Android;
+		buildPlayerOptions.options = BuildOptions.None;
+
+		BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+		BuildSummary summary = report.summary;
+
+		if (summary.result == BuildResult.Succeeded)
+		{
+			// We must use UnityEngine.Debug as we are using System.Diagnositics in order to run an external .exe file
+			UnityEngine.Debug.Log("Build succeeded!");
+			// Copy the file to the server (this is easier for the Android device to download from)
+			File.Copy(@"Build\Android\" + Application.productName + ".apk", @"\\192.168.1.250\ServerDisk1\_Build\Android\"
+				+ Application.productName + ".apk", true);
+		}
+
+		if (summary.result == BuildResult.Failed)
+		{
+			UnityEngine.Debug.Log("***ANDROID BUILD FAILED!***");
+		}
+
+		// If the build succeeds run the Android Emulator
+		else
+			InstallAPKOnEmulator();
+	}
+
+	[MenuItem("Build/Send APK to Emulator")]
+	public static void InstallAPKOnEmulator()
+	{
+		var previousAPKFileLocation = new DirectoryInfo(@"Build\Android\");
+		// Install the APK via ADB (use the same version of ADB that Genymotion [emulator] is using)
+		var installAPK = new ProcessStartInfo(@"C:\Program Files\Genymobile\Genymotion\tools\adb.exe");
+		// 'install -r' will overwrite any previous installation on Android
+		installAPK.Arguments = " install -r " + previousAPKFileLocation + Application.productName + ".apk";
+
+		// Wait for the APK to install
+		var installing = Process.Start(installAPK);
+		installing.WaitForExit();
+
+		// Run the APK in the Android Emulator (use the same version of ADB that Genymotion [emulator] is using)
+		var runAPK = new ProcessStartInfo(@"C:\Program Files\Genymobile\Genymotion\tools\adb.exe");
+		// The company name must be lowercase and have no underscores (the product name case stays the same [minus the '_'])
+		var packageName = "com." + Application.companyName.ToLower().Replace("_", "") + "." + Application.productName.Replace("_", "");
+		runAPK.Arguments = " shell monkey -p " + packageName + " -c android.intent.category.LAUNCHER 1";
+
+		Process.Start(runAPK);
+		// It is too difficult to give the window focus via the Windows API, just click the window instead
+		// If you REALLY need the window to 'pop up' then just build a Windows version instead
+	}
+
+	// If this folder defaults to the 'user' folder then *** You haven't built your game yet ***
+	[MenuItem("Build/Open Windows Build Folder")]
+	public static void OpenWindowsBuildFolder()
+	{
+		// Due to an oddity in RevealInFinder we need to put a containing folder address in the folder we want to navigate
+		// to otherwise it will just take us to the Build folder
+		EditorUtility.RevealInFinder(@"Build\Windows\MonoBleedingEdge");
+	}
+
+	[MenuItem("Build/Windows")]
+	public static void WindowsBuild()
+	{
+		BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+		buildPlayerOptions.scenes = FindActiveScenesInBuildSettings();
+		buildPlayerOptions.locationPathName = @"Build\Windows\" + Application.productName + ".exe";
+		buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+		buildPlayerOptions.options = BuildOptions.None;
+
+		// Resolution & Monitor Build
+		// ==========================
+		// Unfortunately you cannot seem to build your game to the left monitor without first setting it up within the Build Settings first
+		// 
+		// Change Resolution Settings then go to Edit -> Project Settings -> Player Settings -> Resolution & Presentation
+		// Set to Windowed, set Fullscreen: Windowed, this will give you access to the Resolution settings
+		// Build the game once, this will allow you to select which display you want it built on
+		// You can then disable the Resolution Dialog at the start of the game select 'Display Resolution Dialog' in the Player Settings
+		// It will remember your settings for your next Build via this script
+
+		BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+		BuildSummary summary = report.summary;
+
+		if (summary.result == BuildResult.Succeeded)
+		{
+			Debug.Log("Build succeeded!");
+		}
+
+		if (summary.result == BuildResult.Failed)
+		{
+			Debug.Log("*** WINDOWS BUILD FAILED! ***");
+		}
+	}
+
+	[MenuItem("Build/Build and Run _F5")]
+	public static void WindowsBuildAndRun()
+	{
+		// Save any changes made to the Scene before Building
+		EditorSceneManager.MarkAllScenesDirty();
+		EditorSceneManager.SaveOpenScenes();
+
+		BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+		buildPlayerOptions.scenes = FindActiveScenesInBuildSettings();
+		buildPlayerOptions.locationPathName = @"Build\Windows\" + Application.productName + ".exe";
+		buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+
+		BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+		BuildSummary summary = report.summary;
+
+		if (summary.result == BuildResult.Succeeded)
+		{
+			UnityEngine.Debug.Log("Build succeeded!");
+			// *** Option to launch Playmode in the Unity Editor after a successful build
+			//EditorApplication.ExecuteMenuItem("Edit/Play");
+
+			Process.Start(@"Build\Windows\" + Application.productName + ".exe");
+			// *** FOR MULTIPLAYER TESTING *** Uncomment these lines if you need to test a multiplayer game
+			//RunMultipleInstances();
+		}
+
+		if (summary.result == BuildResult.Failed)
+		{
+			UnityEngine.Debug.Log("*** WINDOWS BUILD FAILED! ***");
+		}
+	}
+
+	[MenuItem("Build/Run Multiple Instances _F12")]
+	public static void RunMultipleInstances()
+	{
+		Process.Start(@"Build\Windows\" + Application.productName + ".exe");
+		Process.Start(@"Build\Windows\" + Application.productName + ".exe");
+	}
+
+	public static string[] FindActiveScenesInBuildSettings()
+	{
+		List<string> buildSceneList = new List<string>();
+
+		foreach (var scene in EditorBuildSettings.scenes)
+		{
+			if (scene.enabled)
+				buildSceneList.Add(scene.path);
+		}
+
+		return buildSceneList.ToArray();
+	}
+}
---
+++ Assets/Editor/ToolsMenu.cs
@@ -0,0 +1,164 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Reflection;
+using System.IO;
+
+public class ToolsMenu
+{
+	// Examples on how to use MenuItem: https://unity3d.com/learn/tutorials/topics/interface-essentials/unity-editor-extensions-menu-items?playlist=17090
+	// Modifier Keys Reference: https://unity3d.com/learn/tutorials/topics/interface-essentials/unity-editor-extensions-menu-items?playlist=17090
+
+	// *** Modifier Keys ***
+	// [CTRL]	%
+	// [Shift]	#
+	// [Alt]	&
+	// [Arrow keys] LEFT/RIGHT/UP/DOWN 
+	//
+	// F1…F2 – F keys
+	// HOME, END, PGUP, PGDN
+	// _ (no key combination, just use the key itself)
+	// Just hit F5 => _F5
+	//
+	// [Space] is represented by 'SPACE' (previously it was '_ ')
+	//
+	// [MenuItem("Tools/Open Persistent Data Path")]
+	// This creates a new Menu Item called 'Tools' with 'Open Persistent Data Path' as an option
+
+	[MenuItem("Tools/Open Persistent Data Path")]
+	private static void OpenPersistentDataPath()
+	{
+		// Due to an oddity in RevealInFinder we need to put a containing folder address in the folder we want to navigate
+		// to otherwise it will just take us to the Company Name folder
+		EditorUtility.RevealInFinder(@"C:\Users\MSI\AppData\LocalLow\" + Application.companyName + @"\" 
+			+ Application.productName + @"\Unity");
+
+		// Persistant Data Path Locations
+		//
+		// Windows:
+		// %userprofile%\AppData\LocalLow\<companyName>\<packageName>
+		//
+		// Android:
+		// /storage/emulated/0/Android/data/<packageName>/files
+	}
+
+	[MenuItem("Tools/Clear PlayerPrefs + Save Slots")]
+	static void ClearPlayerPrefsFile()
+	{
+		PlayerPrefs.DeleteAll();
+
+		var saveSlots = new DirectoryInfo(@"C:\Users\MSI\AppData\LocalLow\" + Application.companyName + @"\"
+			+ Application.productName);
+
+		if (saveSlots.Exists)
+		{
+			var filePaths = saveSlots.GetFiles("*.*");
+
+			foreach (var file in filePaths)
+			{
+				file.Delete();
+			}
+		}
+	}
+
+	[MenuItem("Tools/Pause Editor _PGUP")]
+	private static void PauseEditor()
+	{
+		// Toggle between pause & play
+		EditorApplication.isPaused = !EditorApplication.isPaused;
+	}
+
+	[MenuItem("Tools/Toggle Grid %G")]
+	private static void ToggleGrid()
+	{
+		// Toggle the Grid
+		ShowGrid = !ShowGrid;
+	}
+
+	// %PGUP => Hit [Ctrl + PageUp]
+	[MenuItem("Tools/Toggle Auto Refresh %PGUP")]
+	private static void ToggleAutoRefresh()
+	{
+		// Toggle between AutoRefresh & Manual Refresh
+		if (EditorPrefs.GetBool("kAutoRefresh") == true)
+			EditorPrefs.SetBool("kAutoRefresh", false);
+
+		else
+			EditorPrefs.SetBool("kAutoRefresh", true);
+	}
+
+	// taken from: http://answers.unity3d.com/questions/282959/set-inspector-lock-by-code.html
+	[MenuItem("Tools/Toggle Inspector Lock %SPACE")] 
+	static void ToggleInspectorLock()
+	{
+		// This code only works for one Inspector Window, if you have multiple it will only lock one
+		// If you close the 'wrong' Inspector Window it will appear that this code no longer works
+		// but if you open another Inspector Window you will see that it should work for one of them
+		ActiveEditorTracker.sharedTracker.isLocked = !ActiveEditorTracker.sharedTracker.isLocked;
+		ActiveEditorTracker.sharedTracker.ForceRebuild();
+	}
+
+	[MenuItem("Tools/Toggle Always Show Colliders 2D %HOME")]
+	static void ToggleAlwaysShowColliders2D()
+	{
+		Physics2D.alwaysShowColliders = !Physics2D.alwaysShowColliders;
+	}
+
+	[MenuItem("Tools/Play Audio Clip %#SPACE")]
+	//[MenuItem("Tools/Play Audio Clip _SPACE")]	// You can uncomment this when flipping through audio files but you 
+													// shouldn't keep 'space' as a hotkey as you will need space to name things
+	public static void PlayClip()
+	{
+		System.Reflection.Assembly unityEditorAssembly = typeof(AudioImporter).Assembly;
+		System.Type audioUtilClass = unityEditorAssembly.GetType("UnityEditor.AudioUtil");
+		System.Reflection.MethodInfo method = audioUtilClass.GetMethod(
+			"PlayClip",
+			System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public,
+			null,
+			new System.Type[] { typeof(AudioClip), typeof(int), typeof(bool) },
+			null
+		);
+		method.Invoke(
+			null,
+			new object[] { Selection.activeObject, 0, false }
+		);
+	}
+
+	[MenuItem("Tools/Clear Parent %#Z")]
+	static void ClearParent()
+	{
+		if (Selection.activeTransform.parent != null)
+			Selection.activeTransform.transform.parent = null;
+	}
+
+	// Show Grid Codes
+
+	private static Type m_annotationUtility;
+	private static PropertyInfo m_showGridProperty;
+
+	private static PropertyInfo ShowGridProperty
+	{
+		get
+		{
+			if (m_showGridProperty == null)
+			{
+				m_annotationUtility = Type.GetType("UnityEditor.AnnotationUtility,UnityEditor.dll");
+				m_showGridProperty = m_annotationUtility.GetProperty("showGrid", BindingFlags.Static | BindingFlags.NonPublic);
+			}
+			return m_showGridProperty;
+		}
+	}
+
+	public static bool ShowGrid
+	{
+		get
+		{
+			return (bool)ShowGridProperty.GetValue(null, null);
+		}
+
+		set
+		{
+			ShowGridProperty.SetValue(null, value, null);
+		}
+	}
+}
