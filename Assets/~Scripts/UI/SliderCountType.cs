023cc5bcd1b7c8feb08170694614ce74c729de2d
Cathal McCosker <cathal.mccosker@gmail.com>
2019-12-30 16:21:31 -0800

    AutoCommit

--- Assets/~Scripts/UI/SliderCountType.cs
+++ Assets/~Scripts/UI/SliderCountType.cs
@@ -15,26 +15,18 @@ public class SliderCountType : MonoBehaviour
 
     private void UpdateSliderTextEVH()
     {
-        if (countType == "Comrade")
-            GetComponentInChildren<TextMeshProUGUI>().text = LevelSessionData.Singleton.numberOfComrades.ToString();
-
-        if (countType == "Farmer")
-            GetComponentInChildren<TextMeshProUGUI>().text = LevelSessionData.Singleton.numberOfFarmers.ToString();
-
-        if (countType == "Potato")
-            GetComponentInChildren<TextMeshProUGUI>().text = (LevelSessionData.Singleton.numberOfFarmers * 3).ToString();
-
-        if (countType == "Commune")
-            GetComponentInChildren<TextMeshProUGUI>().text = LevelSessionData.Singleton.numberOfCommnues.ToString();
-
-        if (countType == "CommuneFarmer")
-            GetComponentInChildren<TextMeshProUGUI>().text = (LevelSessionData.Singleton.numberOfCommuneFarmers * 4).ToString();
+        UpdateSliderText();
     }
 
     [StringDropdown("Comrade", "Farmer", "Potato", "Commune", "CommuneFarmer")]
     public string countType;
 
     private void Start()
+    {
+        UpdateSliderText();
+    }
+
+    private void UpdateSliderText()
     {
         if (countType == "Comrade")
             GetComponentInChildren<TextMeshProUGUI>().text = LevelSessionData.Singleton.numberOfComrades.ToString();
